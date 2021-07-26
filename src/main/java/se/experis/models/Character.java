f2c2cfc33faad892bf274279fa930287bb09d660
Adam Joulak <adam.joulak@se.experis.com>
2021-07-26 15:20:44 +0200

    character

--- src/main/java/se/experis/models/Character.java
+++ src/main/java/se/experis/models/Character.java
@@ -1,4 +1,82 @@
 package se.experis.models;
 
+import com.fasterxml.jackson.annotation.*;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Character {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "alias")
+    private String alias;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "picture")
+    private String pictureURL;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_library",
+            joinColumns = {@JoinColumn(name = "library_id")},
+            inverseJoinColumns = {@JoinColumn(name = "book_id")}
+    )
+    public List<Movie> movies;
+
+    @JsonGetter("books")
+    public List<String> books() {
+        return movies.stream()
+                .map(book -> {
+                    return "/api/v1/books/" + movies.getId();
+                }).collect(Collectors.toList());
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPictureURL() {
+        return pictureURL;
+    }
+
+    public void setPictureURL(String pictureURL) {
+        this.pictureURL = pictureURL;
+    }
 }
