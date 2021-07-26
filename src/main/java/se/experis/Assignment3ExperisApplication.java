fa6b660cb939b18443b48f64f773f3c5dba1c034
wrang308 <markus.wrang_94@hotmail.com>
2021-07-26 14:12:41 +0200

    first commit

---
+++ src/main/java/se/experis/Assignment3ExperisApplication.java
@@ -0,0 +1,13 @@
+package se.experis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3ExperisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment3ExperisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/experis/Assignment3ExperisApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3ExperisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
