ece2af8e0ee601a7a00b9b8e4aa45b45d428a98b
wrang308 <markus.wrang_94@hotmail.com>
2021-07-26 15:08:51 +0200

    added stuff to the movie and Franchise model

--- src/main/java/se/experis/models/Franchise.java
+++ src/main/java/se/experis/models/Franchise.java
@@ -1,7 +1,59 @@
 package se.experis.models;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonGetter;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Franchise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+
+    @OneToMany
+    @JoinColumn(name = "id")
+    List<Movie> movies;
+
+    @JsonGetter("movies")
+    public List<String> movies() {
+        if(movies != null) {
+            return movies.stream()
+                    .map(movie -> {
+                        return "/api/v1/books/" + movie.getId();
+                    }).collect(Collectors.toList());
+        }
+        return null;
+    }
+
 }
--- src/main/java/se/experis/models/Movie.java
+++ src/main/java/se/experis/models/Movie.java
@@ -1,4 +1,108 @@
 package se.experis.models;
+import com.fasterxml.jackson.annotation.JsonGetter;
 
+import javax.persistence.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String movieTitle;
+    private String genre;
+    private String postalCode;
+    private String director;
+    private String picture;
+    private String trailer;
+
+    @ManyToOne
+    @JoinColumn(name = "franchise")
+    public Franchise franchise;
+
+    @JsonGetter("franchise")
+    public String franchise() {
+        if(franchise != null){
+            return "/api/v1/franchise/" + franchise.getId();
+        }else{
+            return null;
+        }
+    }
+
+    // Book entity
+    @ManyToMany(mappedBy = "characters")
+    public List<Character> characters;
+
+    @JsonGetter("characters")
+    public List<String> librariesGetter() {
+        if(characters != null){
+            return characters.stream()
+                    .map(library -> {
+                        return "/api/v1/libraries/" + characters.getId();
+                    }).collect(Collectors.toList());
+        }
+        return null;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
+
+
+
 }
