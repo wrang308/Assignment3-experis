9a4e3652f308c1cc62fa86a9c55458cd3a135b56
Adam Joulak <adam.joulak@se.experis.com>
2021-07-30 09:52:06 +0200

    kommentarer

--- src/main/java/se/experis/controllers/CharacterController.java
+++ src/main/java/se/experis/controllers/CharacterController.java
@@ -17,26 +17,31 @@ public class CharacterController {
     @Autowired
     private CharacterService characterService;
 
+    //Makes a request to characterservice and returns getting a list of all characters
     @GetMapping()
     public ResponseEntity<List<Character>> getAllCharacters(){
         return characterService.getAllCharacters();
     }
 
+    //Makes a request to characterservice for getting a specific character with given id
     @GetMapping("/{id}")
     public ResponseEntity<Character> getCharacter(@PathVariable Long id){
         return characterService.getCharacter(id);
     }
 
+    //Makes a post-request to characterservice to add/create new character
     @PostMapping
     public ResponseEntity<Character> addCharacter(@RequestBody Character character){
         return characterService.addCharacter(character);
     }
 
+    //Maes a request to characterservice for updating an existing character
     @PutMapping("/")
     public ResponseEntity<Character> updateCharacter( @RequestBody Character character){
         return characterService.updateCharacter(character);
     }
 
+    //Maes a request to characterservice for deleting an existing character
     @DeleteMapping("/{id}")
     public ResponseEntity<Character> deleteCharacter(@PathVariable Long id) {
         return characterService.deleteCharacter(id);
